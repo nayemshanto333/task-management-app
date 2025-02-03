7133d931c80715eda9d3abf1368e3af8b384efb4
nayemshanto333 <nayemshantobd@gmail.com>
2025-02-04 01:07:26 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,122 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+type Ttask = { title: string; details: string };
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tasks, setTasks] = useState<Ttask[]>([]);
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [edit, setEdit] = useState<number | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const addTask = () => {
+    if (title.trim() === "" || details.trim() === "") return;
+
+    if (edit !== null) {
+      const updateTask = [...tasks];
+      updateTask[edit] = { title, details };
+      setTasks(updateTask);
+      setEdit(null);
+    } else {
+      setTasks([...tasks, { title, details }]);
+    }
+
+    setDetails("");
+    setTitle("");
+  };
+
+  const editTask = (index: number) => {
+    setTitle(tasks[index].title);
+    setDetails(tasks[index].details);
+    setEdit(index);
+  };
+
+  const removeTask = (index: number) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <main className="flex justify-center items-center h-screen bg-sky-200 px-2.5 mx-auto">
+      <div className="w-full max-w-screen-xl mx-auto bg-sky-300 py-10 px-5 rounded-lg">
+        <h1 className=" text-xl md:text-3xl font-bold text-sky-600 text-center">
+          Task Management Application
+        </h1>
+
+        {/* Task length  */}
+        <p className="text-center mt-2 font-bold text-slate-500 text-lg">
+          Total Task: {tasks.length.toLocaleString()}
+        </p>
+        <hr className="my-3" />
+
+        {/* Input section */}
+
+        <div className="flex flex-col md:flex-row justify-between items-center gap-10 ">
+          <div className="flex flex-col w-full md:w-1/2 mx-auto  sm:p-5 space-y-2 ">
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title Here..."
+              className="text-[14px] md:text-base px-4 py-2 border border-slate-400 bg-sky-100 focus:outline-none focus:ring focus:ring-indigo-300 rounded"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <input
+              type="text"
+              value={details}
+              onChange={(e) => setDetails(e.target.value)}
+              placeholder="Details Here..."
+              className="text-[14px] md:text-base px-4 py-2 border border-slate-400 bg-sky-100 focus:outline-none focus:ring focus:ring-indigo-300 rounded"
+            />
+            <button
+              onClick={addTask}
+              className={`text-[14px] md:text-base bg-indigo-500 text-white  px-6 py-2 active:translate-y-0.5 rounded shadow-lg`}
+            >
+              {edit !== null ? "Update Task" : "Add New Task"}
+            </button>
+          </div>
+          {/* Output section */}
+          <div className="w-full md:w-1/2 mx-auto sm:px-5 space-y-2 ">
+            {tasks.length > 0 ? (
+              tasks.map((task, i) => (
+                <div
+                  key={i}
+                  className="bg-sky-100 p-5 rounded-md space-y-1 shadow-md"
+                >
+                  <h1 className="font-bold text-base md:text-xl">
+                    {task.title}
+                  </h1>
+                  <p className="text-[14px] md:text-base pb-2">
+                    {task.details}
+                  </p>
+                  <div className="flex items-center gap-2 md:gap-4 ">
+                    {/* Edit Task */}
+
+                    <button
+                      onClick={() => editTask(i)}
+                      className="bg-green-500 rounded px-3 py-1 text-white text-xs sm:text-[14px]"
+                    >
+                      Edit
+                    </button>
+
+                    {/* Remove Task */}
+
+                    <button
+                      onClick={() => removeTask(i)}
+                      className="bg-red-500 rounded px-3 py-1 text-white text-xs sm:text-[14px]"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className=" text-sky-800 text-center text-base sm:text-lg">
+                No Task Avilable
+              </p>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
